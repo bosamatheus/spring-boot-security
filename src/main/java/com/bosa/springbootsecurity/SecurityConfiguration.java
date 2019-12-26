ae4442ea69297a6b21429b1d6f0fa0c3ccbab538
Bosa <mfbs@gft.com>
2019-12-26 17:14:04 -0300

    Using default schema and populating manually with users

--- src/main/java/com/bosa/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/com/bosa/springbootsecurity/SecurityConfiguration.java
@@ -1,27 +1,39 @@
 package com.bosa.springbootsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Set your configuration on the auth object
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
+        // Configuring Authentication
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(
+                    User.withUsername("user")
+                    .password("pass")
+                    .roles("USER")
+            )
+            .withUser(
+                    User.withUsername("admin")
+                            .password("pass")
+                            .roles("ADMIN")
+            );
     }
 
     @Bean
@@ -31,6 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Configuring Authorization
         http.authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
